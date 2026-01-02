02d3a36bbd0994b7c03912420f65f281b8bcb6c3
Mohit Soni <mohit.soni01@cantire.com>
2026-01-02 11:14:48 +0530

    Updated some security related things

--- security-service/src/main/java/com/example/security_service/exception/GlobalExceptionHandler.java
+++ security-service/src/main/java/com/example/security_service/exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.example.security_service.exception;
 
+import io.jsonwebtoken.*;
+import jakarta.servlet.http.*;
+import lombok.extern.slf4j.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.*;
+
+@Slf4j
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidToken(ExpiredJwtException ex,
+                                                            HttpServletRequest request) {
+        log.error("Validation error: {}", ex.getMessage(), ex);
+        ErrorResponse body = new ErrorResponse(Instant.now(), HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
 }
